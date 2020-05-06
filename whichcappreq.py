106accdb4dccdb11d8061caafc4c0013a4f5c4d4
Chamod <48453186+luckycham@users.noreply.github.com>
2020-05-05 23:05:27 -0600

    Initial Commit
    Able to search for countries. Prints out in console when items are selected from search

---
+++ whichcappreq.py
@@ -0,0 +1,40 @@
+import requests
+
+
+class WeatherRequests:
+    def __init__(self):
+        self.url = "https://www.metaweather.com/api/"
+
+    def search_city_name(self, name):
+        """
+            Searches for a list of cities matching the given name
+
+            :param name: Name to search for
+            :return: A list of cities containing the matching name
+            """
+        try:
+            tmp = self.url
+            tmp += 'location/search/'
+            payload = {'query': name}
+            response = requests.get(tmp, params=payload)
+            weather = response.json()
+
+            return weather
+
+        except ValueError:
+            return None
+
+    def get_weather(self, woeid):
+        """
+        Returns weather information of the city matching given woeid
+
+        :param woeid: Where on earth id
+        :return: A list of weather information about the matching woeid
+        """
+        tmp = self.url
+        tmp += 'location/'
+        tmp += str(woeid)
+        response = requests.get(tmp)
+        weather = response.json()
+
+        return weather
