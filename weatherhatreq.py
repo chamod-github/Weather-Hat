84ee5e595856ab0da393ce00e4854dd6de0a0a77
Chamod <48453186+luckycham@users.noreply.github.com>
2020-05-11 13:35:35 -0600

    GUI initialize in the class and Requests are functions
    GUI now initialized inside of the class intialization. API is no longer an object.

--- weatherhatreq.py
+++ weatherhatreq.py
@@ -1,47 +1,52 @@
 import requests
 
+url = "https://www.metaweather.com/api/"
 
-class WeatherHatRequests:
-    def __init__(self):
-        self.url = "https://www.metaweather.com/api/"
 
-    def search_city_name(self, name):
-        """
-        Searches for a list of cities matching the given name
+def search_city_name(name):
+    """
+    Searches for a list of cities matching the given name
 
-        :param name: Name to search for
-        :return: A list of cities containing the matching name
-        
-        """
-        try:
-            tmp = self.url
-            tmp += 'location/search/'
-            payload = {'query': name}
-            response = requests.get(tmp, params=payload)
-            weather = response.json()
+    :param name: Name to search for
+    :return: A list of cities containing the matching name
 
-            return weather
-
-        except ValueError:
-            return None
-
-    def get_weather(self, woeid):
-        """
-        Returns weather information of the city matching given woeid
-
-        :param woeid: Where on earth id
-        :return: A list of weather information about the matching woeid
-        """
-        tmp = self.url
-        tmp += 'location/'
-        tmp += str(woeid)
-        response = requests.get(tmp)
+    """
+    try:
+        tmp = url
+        tmp += 'location/search/'
+        payload = {'query': name}
+        response = requests.get(tmp, params=payload)
         weather = response.json()
 
         return weather
 
-    def get_weather_abbr(self, woeid):
-        tmp = self.get_weather(woeid)['consolidated_weather'][0]['weather_state_abbr']
-        if tmp:
-            return tmp
+    except ValueError:
         return None
+
+
+def get_weather(woeid):
+    """
+    Returns weather information of the city matching given woeid
+
+    :param woeid: Where on earth id
+    :return: A list of weather information about the matching woeid
+    """
+    tmp = url
+    tmp += 'location/'
+    tmp += str(woeid)
+    response = requests.get(tmp)
+    weather = response.json()
+
+    return weather
+
+
+def get_weather_abbr(woeid):
+    """
+
+    :param woeid: woeid of the location
+    :return: Weather abbreviation of the woeid
+    """
+    tmp = get_weather(woeid)['consolidated_weather'][0]['weather_state_abbr']
+    if tmp:
+        return tmp
+    return None
